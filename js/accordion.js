88fe03275fb1df1632612e959e0690b6f423f23b
displaygreat <displaygreat@gmail.com>
2021-05-19 16:24:32 +0300

    smooth scroll and tabs

--- js/accordion.js
+++ js/accordion.js
@@ -2,28 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const featureLinkElems = document.querySelectorAll(".feature__link");
   const featureSubElems = document.querySelectorAll(".feature-sub");
 
-  // for (let i = 0; i < featureLinkElems.length; i++) {
-  //   featureLinkElems[i].addEventListener("click", () => {
-  //     featureSubElems[i].classList.toggle("hidden");
-  //     featureLinkElems[i].classList.toggle("feature__link_active");
-  //   });
-  // }
   featureLinkElems.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      if (btn.classList.contains("feature__link_active")) {
-        featureSubElems[index].classList.add("hidden");
-        btn.classList.remove("feature__link_active");
-      } else {
-        featureSubElems.forEach((featureSubElem) => {
-          featureSubElem.classList.add("hidden");
-        });
-        featureLinkElems.forEach((featureLinkElem) => {
-          featureLinkElem.classList.remove("feature__link_active");
-        });
+      // if (btn.classList.contains("feature__link_active")) {
+      //   featureSubElems[index].classList.add("hidden");
+      //   btn.classList.remove("feature__link_active");
+      // } else {
+      featureSubElems.forEach((featureSubElem) => {
+        featureSubElem.classList.add("hidden");
+      });
+      featureLinkElems.forEach((featureLinkElem) => {
+        featureLinkElem.classList.remove("feature__link_active");
+      });
 
-        featureSubElems[index].classList.remove("hidden");
-        btn.classList.add("feature__link_active");
-      }
+      featureSubElems[index].classList.toggle("hidden");
+      btn.classList.toggle("feature__link_active");
+      // }
     });
   });
 });
