566f5bd70be209387c091aa0f24ef171d33b9399
displaygreat <displaygreat@gmail.com>
2021-05-21 01:38:00 +0300

    desable scroll when modal opened

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,30 @@
-const moreElems = document.querySelectorAll(".more");
+//variant 1 to open modal using event delegation
 const modalElem = document.querySelector(".modal");
+//variant 2 to open modal
+// const moreElems = document.querySelectorAll(".more");
+const designBlock = document.querySelector(".design-block");
 
 const openModal = () => {
   modalElem.classList.remove("hidden");
+  disableScroll();
 };
 const closeModal = () => {
   modalElem.classList.add("hidden");
+  enableScroll();
 };
 
-moreElems.forEach((elem) => {
-  elem.addEventListener("click", openModal);
+//variant 1 to open modal using event delegation
+designBlock.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.matches(".more")) {
+    openModal();
+  }
 });
+
+//variant 2 to open modal
+// moreElems.forEach((elem) => {
+//   elem.addEventListener("click", openModal);
+// });
 
 modalElem.addEventListener("click", (event) => {
   const target = event.target;
